940250f99155bd328eb7e7c647ce1a25abd92962
Chris Kolenko <chris@webcanvas.com.au>
2025-01-23 16:40:08 +1100

    Update API

--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -1,7 +1,12 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './Benefit';
+export * from './Campaign';
 export * from './ErrorResponse';
 export * from './Installation';
+export * from './Membership';
 export * from './PostInstallationRequest';
 export * from './PostInstallationResponse';
 export * from './PutInstallationRequest';
+export * from './Tier';
+export * from './User';
